8464c2af392271d39c6c76ef5154382eed3ae5de
Mitchell <computers1055@gmail.com>
2018-05-22 18:10:24 -0400

    Added method to replace tokens in a dictonary

--- token_replacer.py
+++ token_replacer.py
@@ -1,6 +1,8 @@
 import os
 import re
-import json
+
+
+TOKEN_REGEX = re.compile(r"\[\[.+\]\]")
 
 
 def replace_tokens_in_file(in_file, tokens, out_file=None, delete_after=False):
@@ -9,16 +11,7 @@ def replace_tokens_in_file(in_file, tokens, out_file=None, delete_after=False):
     with open(in_file, "r") as file:
         content = file.read()
 
-    pattern = re.compile(r"\[\[.+\]\]")
-
-    # check if we have values for all of the tokens in the file
-    # if not throw an exception
-    for match in re.findall(pattern, content):
-        key = remove_delimiters_from_key(match)
-        if key not in tokens:
-            raise Exception("Unknown key: " + key)
-
-    content_replaced = pattern.sub(lambda x: str(tokens[remove_delimiters_from_key(x.group())]), content)
+    content_replaced = replace_tokens_in_string(content, tokens)
 
     with open(out_file, "w") as file:
         file.write(content_replaced)
@@ -27,6 +20,24 @@ def replace_tokens_in_file(in_file, tokens, out_file=None, delete_after=False):
         os.remove(in_file)
 
     return True
+
+
+def replace_tokens_in_dict(dict, tokens):
+    out_dict = dict()
+
+    for key, value in out_dict.items():
+        out_dict[key] = replace_tokens_in_string(value, tokens)
+
+    return out_dict
+
+
+def replace_tokens_in_string(string, tokens):
+    for match in re.findall(TOKEN_REGEX, string):
+        key = remove_delimiters_from_key(match)
+        if key not in tokens:
+            raise Exception("Unknown key: " + key)
+
+    return TOKEN_REGEX.sub(lambda x: str(tokens[remove_delimiters_from_key(x.group())]), string)
 
 
 def remove_delimiters_from_key(key):
